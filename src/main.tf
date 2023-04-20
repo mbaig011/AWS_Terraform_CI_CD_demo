1090dc49f5e7d75f22dbd6490da87783f876db0b
Mirza Baig <mirza.baig@metasitesdm.com>
2023-04-20 06:35:36 -0500

    init commit

--- src/main.tf
+++ src/main.tf
@@ -1,11 +1,11 @@
 terraform {
-  # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
+    # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
   backend "s3" {
-    bucket         = "cc-tf-state-backend-ci-cd"
+    bucket         = "cc-tf-state-backend-ci-marsha"
     key            = "tf-infra/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-locking"
+    dynamodb_table = "terraform-state-locking-marsha"
     encrypt        = true
   }
 
@@ -24,15 +24,5 @@ provider "aws" {
 
 module "tf-state" {
   source      = "./modules/tf-state"
-  bucket_name = "cc-tf-state-backend-ci-cd"
-}
-
-module "vpc-infra" {
-  source = "./modules/vpc"
-
-  # VPC Input Vars
-  vpc_cidr             = local.vpc_cidr
-  availability_zones   = local.availability_zones
-  public_subnet_cidrs  = local.public_subnet_cidrs
-  private_subnet_cidrs = local.private_subnet_cidrs
+  bucket_name = "cc-tf-state-backend-ci-marsha"
 }
